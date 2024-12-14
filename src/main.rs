cbe97bab8bdbfe67079f1ae43e0b30b0e684b04b
Ben Hess <pilotben@comcast.net>
2024-12-13 20:57:11 -0800

    added indexing and scalars

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,9 @@ use error::TensorError;
 use tensor::Tensor;
 
 fn main() -> Result<(), TensorError> {
-    let zeros = Tensor::from_shape(0f32, [2, 2, 2]);
-    let array = Tensor::from_array([1, 2, 3, 4, 5, 6]);
-    println!("{}", zeros);
-    println!("{}", array);
-    println!();
-
-    let reshaped = array.reshape([2, 3])?;
-    let verical = array.reshape([1, 6])?;
-    println!("{}", array);
-    println!("{}", reshaped);
-    println!("{}", verical);
-
+    let t = Tensor::from_array([1, 2, 3, 4]).reshape([2, 2])?;
+    println!("{}", t);
+    println!("{}", t.get(&[1, 1])?);
+    println!("{}", Tensor::scalar(1));
     Ok(())
 }
--- src/tensor.rs
+++ src/tensor.rs
@@ -2,33 +2,80 @@ use std::{cell::RefCell, fmt::Display, rc::Rc};
 
 use crate::error::TensorError;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Tensor<T> {
     data: Rc<RefCell<Vec<T>>>,
+    base_index: usize,
+    size: usize,
     shape: Vec<usize>,
     strides: Vec<usize>
 }
 
 fn get_size_and_strides(shape: &[usize]) -> (usize, Vec<usize>) {
-    let mut strides = Vec::<usize>::with_capacity(shape.len());
+    let mut strides = vec![0 as usize; shape.len()];
     let mut size: usize = 1;
-    for d in shape {
-        strides.push(size);
-        size *= d;
+    for i in (0..shape.len()).rev() {
+        strides[i] = size;
+        size *= shape[i];
     }
     return (size, strides);
 }
 
 impl<T> Tensor<T> {
+    pub fn get_data_index(&self, index: &[usize], tile: bool) -> Result<usize, TensorError> {
+        if index.len() > self.shape.len() {
+            return Err(TensorError::new("index has too many dimensions"));
+        }
+        let mut data_index: usize = self.base_index;
+        for i in 0..index.len() {
+            let mut next_index = index[i];
+            if tile {
+                next_index %= self.shape[i];
+            } else if next_index >= self.shape[i] {
+                return Err(TensorError::new(format!("index {} is out of range for dimension {}", next_index, i)));
+            }
+            data_index += next_index * self.strides[i];
+        }
+        Ok(data_index)
+    }
+
+    pub fn get(&self, index: &[usize]) -> Result<Tensor<T>, TensorError> {
+        let base_index = self.get_data_index(index, false)?;
+        Ok(Tensor {
+            data: self.data.clone(),
+            base_index: base_index,
+            size: self.strides[index.len() - 1],
+            shape: self.shape[index.len()..].to_vec(),
+            strides: self.strides[index.len()..].to_vec()
+        })
+    }
+
+    pub fn rank(&self) -> usize {
+        self.shape.len()
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    pub fn shape(&self) -> &[usize] {
+        self.shape.as_slice()
+    }
+
+    pub fn is_scalar(&self) -> bool {
+        self.rank() == 0
+    }
+
     pub fn reshape<S: Into<Vec<usize>>>(&self, new_shape: S) -> Result<Tensor<T>, TensorError> {
         let _shape: Vec<usize> = new_shape.into();
         let (size, strides) = get_size_and_strides(_shape.as_slice());
-        let (current_size, _) = get_size_and_strides(self.shape.as_slice());
-        if size != current_size {
+        if size != self.size {
             return Err(TensorError::new("new shape cannot be of a different size"))
         }
         Ok(Tensor {
             data: self.data.clone(),
+            base_index: 0,
+            size: self.size,
             shape: _shape,
             strides: strides
         })
@@ -41,6 +88,8 @@ impl<T: Clone> Tensor<T> {
         let (size, strides) = get_size_and_strides(_shape.as_slice());
         Tensor {
             data: Rc::new(RefCell::new(vec![value; size])),
+            base_index: 0,
+            size: size,
             strides: strides,
             shape: _shape
         }
@@ -49,26 +98,38 @@ impl<T: Clone> Tensor<T> {
     pub fn from_array<A: Into<Vec<T>>>(arr: A) -> Tensor<T> {
         let _arr = arr.into();
         Tensor {
+            base_index: 0,
             strides: vec![1],
             shape: vec![_arr.len()],
+            size: _arr.len(),
             data: Rc::new(RefCell::new(_arr))
         }
+    }
+
+    pub fn scalar(value: T) -> Tensor<T> {
+        Self::from_shape(value, &[])
     }
 }
 
 impl<T: Display> Display for Tensor<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let data = self.data.borrow();
+
+        if self.is_scalar() {
+            write!(f, "{}", data[self.base_index])?;
+            return Ok(());
+        }
+
         let mut depth = 0;
-        for i in 0..data.len() {
+        for i in 0..self.size {
             let mut open = if i == 0 { 1 } else { 0 };
-            let mut close = if i + 1 == data.len() { 1 } else { 0 };
-            for s in &self.strides[1..] {
+            let mut close = if i + 1 == self.size { 1 } else { 0 };
+            for s in &self.strides[..self.strides.len() - 1] {
                 if i % s == 0 {
                     open += 1;
                 }
             }
-            for s in &self.strides[1..] {
+            for s in &self.strides[..self.strides.len() - 1] {
                 if (i + 1) % s == 0 {
                     close += 1;
                 }
@@ -76,7 +137,7 @@ impl<T: Display> Display for Tensor<T> {
             for _ in 0..open {
                 write!(f, "[")?;
             }
-            write!(f, "{}", data[i])?;
+            write!(f, "{}", data[self.base_index + i])?;
             if close == 0 {
                 write!(f, ", ")?;
             }
